874e5f433d5c54edf8da8ee055860cd8d6fd6286
Guilherme Silva <guilherme0541@gmail.com>
2024-09-28 12:29:47 -0300

    disable kms key on eks cluster

--- eks/infra/clusterEks.tf
+++ eks/infra/clusterEks.tf
@@ -10,6 +10,9 @@ module "eks_cluster" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  create_kms_key              = false
+  create_cloudwatch_log_group = false
+
    cluster_addons = {
      coredns = {
      most_recent = true
